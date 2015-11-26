c27bb70bc9bfd37fa38283cb92166a9172a39b34
Kimberly Lomibao <lomibaokimberly@gmail.com>
2015-11-26 22:26:32 +0800

    Added one way to display date.

--- GregorianCalendar.java
+++ GregorianCalendar.java
@@ -12,14 +12,17 @@ public class GregorianCalendar {
 
     public static void main (String [] args) {
 
-	//shows the current time
 		Calendar cal = Calendar.getInstance();
 		
-                SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyy hh:mm:ss");
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyy hh:mm:ss");
 		String t1 = sdf.format(cal.getTime());
 		System.out.println("Current Date and Time: " + sdf.format(cal.getTime()));
 
-	//shows the current date
+
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		System.out.println(dateFormat.format(date));
+
     }
 
 }
